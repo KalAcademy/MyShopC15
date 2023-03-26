94b1f5c909caf44e74bb094398f32561b65050f4
Kal <info@kalacademy.org>
2023-03-26 11:13:33 -0700

    Added components

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
+
+const HomeScreen = () => {
+  //this function 
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((p) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={p} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
