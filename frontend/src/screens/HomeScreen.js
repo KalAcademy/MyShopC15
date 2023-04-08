769aeaad35ea2e890d657245e23fc46e4e8e34d6
Kal <info@kalacademy.org>
2023-04-08 11:04:02 -0700

    Integrating frontend with backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,24 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Col, Row } from 'react-bootstrap'
 import axios from 'axios'
 import Product from '../components/Product'
 
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
   useEffect(() => {
-    const fetchProducts = () => {
-      const {data} = axios.get('/api/products')
-
+    const fetchProducts = async () => {
+      const {data} = await axios.get('/api/products')
+      setProducts(data)
     }
-
+    fetchProducts()
   } )
 
   //this function 
   return (
     <>
-      <h1>Latest Products</h1>
+      <h1>Latest Products From Myshop</h1>
       <Row>
         {products.map((p) => (
           <Col sm={12} md={6} lg={4} xl={3}>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useParams} from 'react-router-dom'
 import {Row, Col, Image, Card, Button, ListGroup} from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 const ProductScreen = () => {
   const params = useParams();
-  const product = products.find(p => p._id === params.id)
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/product/${params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  })
   
   return (
     <>
